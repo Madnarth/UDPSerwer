51dd93a4354b82d37d9fa36295fa01b597b4b21f
Madnarth <corwyn.blade@gmail.com>
2018-03-20 21:08:47 +0100

    Initial commit

---
+++ UDPSerwer/Form1.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace UDPSerwer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label2 = new System.Windows.Forms.Label();
+            this.numPort = new System.Windows.Forms.NumericUpDown();
+            this.lbLogs = new System.Windows.Forms.ListBox();
+            this.bStart = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.numPort)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(171, 182);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 13);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "Port:";
+            // 
+            // numPort
+            // 
+            this.numPort.Location = new System.Drawing.Point(206, 179);
+            this.numPort.Maximum = new decimal(new int[] {
+            65535,
+            0,
+            0,
+            0});
+            this.numPort.Name = "numPort";
+            this.numPort.Size = new System.Drawing.Size(66, 20);
+            this.numPort.TabIndex = 12;
+            // 
+            // lbLogs
+            // 
+            this.lbLogs.FormattingEnabled = true;
+            this.lbLogs.Location = new System.Drawing.Point(12, 12);
+            this.lbLogs.Name = "lbLogs";
+            this.lbLogs.Size = new System.Drawing.Size(260, 134);
+            this.lbLogs.TabIndex = 14;
+            // 
+            // bStart
+            // 
+            this.bStart.Location = new System.Drawing.Point(12, 172);
+            this.bStart.Name = "bStart";
+            this.bStart.Size = new System.Drawing.Size(75, 23);
+            this.bStart.TabIndex = 15;
+            this.bStart.Text = "Uruchom";
+            this.bStart.UseVisualStyleBackColor = true;
+            this.bStart.Click += new System.EventHandler(this.bStart_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 211);
+            this.Controls.Add(this.bStart);
+            this.Controls.Add(this.lbLogs);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.numPort);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.numPort)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown numPort;
+        private System.Windows.Forms.ListBox lbLogs;
+        private System.Windows.Forms.Button bStart;
+    }
+}
+
---
+++ UDPSerwer/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UDPSerwer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void bStart_Click(object sender, EventArgs e)
+        {
+            int port = (int)numPort.Value;
+            IPEndPoint zdalnyIP = new IPEndPoint(IPAddress.Any, 0);
+            try
+            {
+                UdpClient serwer = new UdpClient(port);
+                Byte[] odczyt = serwer.Receive(ref zdalnyIP);
+                string dane = Encoding.ASCII.GetString(odczyt);
+                lbLogs.Items.Add(dane);
+                serwer.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Błąd");
+            }
+        }
+    }
+}
